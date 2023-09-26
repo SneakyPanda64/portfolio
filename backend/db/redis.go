dba3ae06d3c434bbe3a45a0cf4a7e82df7971416
Alexander <alexander.j.heather@gmail.com>
2023-09-26 19:19:07 +0100

    .

--- backend/db/redis.go
+++ backend/db/redis.go
@@ -14,10 +14,10 @@ var (
 
 func Connect() error {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "na",
-		Username: os.Getenv("REDIS_USERNAME"),
-		Password: os.Getenv("REDIS_PASSWORD"), // no password set
-		DB:       0,                           // use default DB
+		Addr: "na",
+		// Username: os.Getenv("REDIS_USERNAME"),
+		// Password: os.Getenv("REDIS_PASSWORD"), // no password set
+		DB: 0, // use default DB
 	})
 	logrus.Infof("%s, %s, %s", os.Getenv("REDIS_URI"), os.Getenv("REDIS_USERNAME"), os.Getenv("REDIS_PASSWORD"))
 	err := client.Ping(context.Background()).Err()
