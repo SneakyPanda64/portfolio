410ce8f0a1b2cd5f986d54f88d3b2074f96b733f
sneakypanda64 <alexander.j.heather@gmail.com>
2023-09-26 23:05:25 +0100

    .

--- backend/db/redis.go
+++ backend/db/redis.go
@@ -15,12 +15,11 @@ var (
 func Connect() error {
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_URI"),
-		Username: os.Getenv("REDIS_USERNAME"),
+		Addr: os.Getenv("REDIS_URI"),
+		// Username: os.Getenv("REDIS_USERNAME"),
 		Password: os.Getenv("REDIS_PASSWORD"), // no password set
 		DB:       0,                           // use default DB
 	})
-	logrus.Infof("%s, %s, %s", os.Getenv("REDIS_URI"), os.Getenv("REDIS_USERNAME"), os.Getenv("REDIS_PASSWORD"))
 	err := client.Ping(context.Background()).Err()
 	if err != nil {
 		return err
